d62581cb35f83e24bbd34a157a0efa75f8f105e8
lshoyfer <lshoyfer@gmail.com>
2022-11-26 16:33:17 -0500

    Write base analysis page

--- app/analysis/[id]/page.js
+++ app/analysis/[id]/page.js
@@ -1,9 +1,40 @@
-'use client'
-import 'client-only';
+import 'server-only';
+import { supabase } from '../../../utils/supabaseClient';
+import styles from '../../../styles/Analysis.module.css';
+import Link from 'next/link';
 
-export default function Analysis({ params, searchParams }) {
-    
-    return (
-      null  
-    );
+// console.log(styles);
+
+async function getAnalysisData(id) {
+  const { data, error } = await supabase
+    .from('analysis')
+    .select()
+    .eq('id', id);
+  return data[0];
+}
+
+export default async function Analysis({ params, searchParams }) {
+  const analysis = await getAnalysisData(params.id);
+  const time = (new Intl.DateTimeFormat('en', { timeStyle: 'short', dateStyle: 'short' }))
+    .format(new Date(analysis.created_at));
+
+  return (
+    <div className={styles.mainContainer}>
+
+      <div className={styles.infoBar}>
+        <div className={styles.title}>{analysis.title}</div>
+        <div className={styles.infoBarMisc}>
+          <span>{analysis.owner ?? 'Public'}</span>
+          <Link className={styles.convos} href='/'>Convos</Link>
+          <span>{time}</span>
+        </div>
+      </div>
+
+      <div className={styles.infoContainer} dangerouslySetInnerHTML={{ __html: analysis.content_html }}/>
+
+      <div className={styles.comments}>
+        Comments Placeholder
+      </div>
+    </div>
+  );
 }
--- utils/supabaseClient.js
+++ utils/supabaseClient.js
@@ -2,4 +2,4 @@ import { createClient } from '@supabase/supabase-js';
 const supabaseURL = process.env.NEXT_PUBLIC_SUPABASE_URL ?? '';
 const supabaseKEY = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY ?? '';
 
-export const supabaseClient = createClient(supabaseURL, supabaseKEY);
+export const supabase = createClient(supabaseURL, supabaseKEY);
