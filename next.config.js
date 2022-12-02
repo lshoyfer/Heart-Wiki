b3843f816989c373ea0e31c53df409ffdfc68d19
lshoyfer <lshoyfer@gmail.com>
2022-12-01 22:15:11 -0500

    Fix enableUndici Next18 warn spam/bug | See next.config.js comment

--- next.config.js
+++ next.config.js
@@ -8,11 +8,21 @@ const nextConfig = {
       {
         protocol: 'https',
         hostname: 'ogqlzchzftdqgeolrbzc.supabase.co',
-        // port: '',
-        // pathname: '/storage/v1/object/public/avatar/**'
       }
     ]
   }
 }
+
+/* TO REMOVE the annoying enableUndici NodeJS 18 warn spam in stdout:
+  node_modules/next/dist/server/config.js
+  line 196
+    Log.warn(`\`enableUndici\` option is unnecessary in Node.js v${NODE_18_VERSION} or greater.`);
+  delete that shit; thank me later
+  or I guess I'm thanking myself...
+  I had a good 30 minutes digging through node_modules/next to find this and
+  silence this horrifically annoying warning (it's a NextJS 18 bug and there
+  is no helpful info online about it due to NextJS 18 being new/experimental 
+  so I had to brute force it like this)
+*/
 
 module.exports = nextConfig;
